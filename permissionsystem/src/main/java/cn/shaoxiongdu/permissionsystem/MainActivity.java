d30e420f7b494da230cf045035c576b95bc3941c
dushaoxiong <dushaoxiong@lixiang.com>
2022-08-02 16:17:34 +0800

    iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii::12313123# [Module:type] A short summary (max line length: 72 characters)
    Flac codec added which is a prerequisite of enabling flac support
    for audio player
    
    Issue-Id: DUMMY-xxxx, DUMMY-xxxx

--- permissionsystem/src/main/java/cn/shaoxiongdu/permissionsystem/MainActivity.java
+++ permissionsystem/src/main/java/cn/shaoxiongdu/permissionsystem/MainActivity.java
@@ -28,35 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
     public static final int REQUEST_LOCATION_CODE = 0x1;
-    public static final int REQUEST_CALL_CODE = 0x2;
-    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadWebView();
         loadPosition();
     }
 
     private void loadPosition() {
         boolean permission = checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
-        if (permission){
-            getLocationManager();
-            showPosition();
-        }else {
+        if (!permission){
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_LOCATION_CODE);
             }
         }
-    }
-
-    private void showPosition() {
-        TextView positionTv = findViewById(R.id.positionTv);
-        @SuppressLint("MissingPermission") Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        positionTv.setText("经度:" + latitude + "\n" + "纬度: " + longitude);
     }
 
     /**
@@ -72,56 +58,12 @@ public class MainActivity extends AppCompatActivity {
             case REQUEST_LOCATION_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "已成功获取到定位权限", Toast.LENGTH_SHORT).show();
-                    getLocationManager();
-                    showPosition();
                 } else {
                     Toast.makeText(this, "您拒绝了授权", Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case REQUEST_CALL_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    call(null);
-                } else {
-                    Toast.makeText(this, "您拒绝了拨号授权", Toast.LENGTH_SHORT).show();
-                }
-                break;
         }
     }
-
-    private void loadWebView() {
-        WebView webView = findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://baidu.com");
-    }
-
-    public void getLocationManager() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            locationManager = getSystemService(LocationManager.class);
-            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                //跳转到手机打开GPS页面
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                //设置完成后返回原来的界面
-                startActivity(intent);
-            }
-        }
-    }
-
-    public void call(View view) {
-        EditText phoneNumberEt = findViewById(R.id.phone_number_et);
-        boolean checkPermission = checkPermission(Manifest.permission.CALL_PHONE);
-        if (checkPermission){
-            String phoneNumber = phoneNumberEt.getText().toString();
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("tel:" + phoneNumber));
-            startActivity(intent);
-        }else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL_CODE);
-            }
-        }
-    }
-
 
     /**
      * 检查权限
